4ab817bd256dd477f760cab32ef674abd4a00f7e
cromega <cr.jrg08@gmail.com>
2022-11-04 13:47:05 -0500

    add LICENSE

--- app/src/main/java/com/example/tablesono/MainActivity.java
+++ app/src/main/java/com/example/tablesono/MainActivity.java
@@ -15,6 +15,24 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+/*
+*   <Tablesono: A Tip Calculator for Android OS.>
+    Copyright (C) <2022>  <Cromega>.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+* * */
+
 public class MainActivity extends AppCompatActivity {
 
     private LinearLayout plates;
